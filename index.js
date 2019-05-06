ea388de2fe1bf212b5a35e071411e3de8845332b
Your Name <you@example.com>
2019-05-06 17:41:30 +0400

    տեքստ այն մասին, թե ինչ փոփոխություններ են արվել

---
+++ index.js
@@ -0,0 +1,43 @@
+// console.log("Hello Node");
+
+// var os = require("os");
+// var message = "The platform is ";
+
+// function main(){
+//     console.log(message + os.platform());
+// }
+// main();
+
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function(req, res){
+//    res.send("Hello world");
+// });
+
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+
+// app.get("/name/:name", function(req, res){
+//     var name = req.params.name;
+//     res.send("<h1>Hello " + name + "</h1>");
+//  });
+ 
+// res.redirect('http://google.com');
+
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){ 
+    res.redirect('http://google.com');
+//    res.send("<h1>Hello world</h1>");
+});
+// app.get("/name/:name", function(req, res){
+//    var name = req.params.name;
+//    res.send("<h1>Hello " + name +"</h1>");
+  
+// });
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});
